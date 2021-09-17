a25ffea4bf8f1dd8efc5afa9b301b68137c36fc0
王辉 <wanghui89757@gmail.com>
2021-09-17 14:51:52 +0800

    init.

---
+++ ReceiptPrinter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReceiptPrinter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
